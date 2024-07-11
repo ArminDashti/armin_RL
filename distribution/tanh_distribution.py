804476b68ab1b4fbed63fdaa3f1aa46e8e31e0ab
Armin <arminonline71@gmail.com>
2024-07-11 21:16:40 +0330

    Update

--- distribution/tanh_distribution.py
+++ distribution/tanh_distribution.py
@@ -1,10 +1,11 @@
-# From https://github.com/aviralkumar2907/CQL/blob/master/d4rl/rlkit/torch/distributions.py
+# https://github.com/aviralkumar2907/CQL/blob/master/d4rl/rlkit/torch/distributions.py
 
 import torch
 from torch.distributions import Distribution, Normal
 
 
-class tanh_normal(Distribution):
+
+class TanhNormal(Distribution):
     def __init__(self, normal_mean, normal_std, epsilon=1e-6):
         self.normal_mean = normal_mean
         self.normal_std = normal_std
@@ -22,12 +23,19 @@ class tanh_normal(Distribution):
 
     def log_prob(self, value, pre_tanh_value=None):
         if pre_tanh_value is None:
-            pre_tanh_value = torch.log((1+value) / (1-value)) / 2
-            
-        return self.normal.log_prob(pre_tanh_value) - torch.log(1 - value * value + self.epsilon)
+            pre_tanh_value = torch.log(
+                (1+value) / (1-value)
+            ) / 2
+        return self.normal.log_prob(pre_tanh_value) - torch.log(
+            1 - value * value + self.epsilon
+        )
 
 
     def sample(self, return_pretanh_value=False):
+        """
+        Gradients will and should *not* pass through this operation.
+        See https://github.com/pytorch/pytorch/issues/4620 for discussion.
+        """
         z = self.normal.sample().detach()
 
         if return_pretanh_value:
@@ -36,12 +44,17 @@ class tanh_normal(Distribution):
             return torch.tanh(z)
 
 
-    def rsample(self, return_pre_tanh_value=False):
-        z = self.normal_mean + self.normal_std * Normal(ptu.zeros(self.normal_mean.size()), 
-                                                         ptu.ones(self.normal_std.size())).sample()
+        z = (
+            self.normal_mean +
+            self.normal_std *
+            Normal(
+                ptu.zeros(self.normal_mean.size()),
+                ptu.ones(self.normal_std.size())
+            ).sample()
+        )
         z.requires_grad_()
 
-        if return_pre_tanh_value:
+        if return_pretanh_value:
             return torch.tanh(z), z
         else:
             return torch.tanh(z)
--- utils.py
+++ utils.py
@@ -3,7 +3,7 @@ import torch.nn as nn
 
 
 # https://github.com/davidbrandfonbrener/onestep-rl/blob/main/utils.py
-def soft_update_params(net, target_net, tau):
+def soft_update(net, target_net, tau):
     for param, target_param in zip(net.parameters(), target_net.parameters()):
         target_param.data.copy_(tau * param.data + (1 - tau) * target_param.data)
         
@@ -13,3 +13,9 @@ def soft_clamp(x, low, high):
     x = torch.tanh(x)
     x = low + 0.5 * (high - low) * (x + 1)
     return x
+
+
+# https://github.com/jakegrigsby/deep_control/blob/master/deep_control/utils.py
+def hard_update(target, source):
+    for target_param, param in zip(target.parameters(), source.parameters()):
+        target_param.data.copy_(param.data)
